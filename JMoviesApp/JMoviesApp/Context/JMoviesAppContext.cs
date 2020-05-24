c7f98a986e5378bf86cf9b9b1516327d81d889f0
javi9400 <javi_mayorga94@hotmail.com>
2020-05-24 17:05:28 -0600

    Creating models and configuring services to use context

---
+++ JMoviesApp/JMoviesApp/Context/JMoviesAppContext.cs
@@ -0,0 +1,23 @@
+﻿using JMoviesApp.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JMoviesApp.Context
+{
+    public class JMoviesAppContext:DbContext
+    {
+        public JMoviesAppContext(DbContextOptions<JMoviesAppContext> context) : base(context)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Customer> Customer { get; set; }
+
+
+
+    }
+}
